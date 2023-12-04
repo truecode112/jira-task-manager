449fb6e49bd5446f1006305e0925878b877a1623
truecode112 <truecoder112@gmaili.com>
2023-12-05 00:06:36 +0300

    first part done

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import itertools
 from datetime import datetime
 from datetime import date
 from functools import cmp_to_key
+from dateutil import parser
 
-logging.basicConfig(format="%(asctime)s \t%(message)s", level=logging.INFO)
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 
 JIRA_API_TOKEN = config('JIRA_API_TOKEN')
+NO_UPDATE_THRESHOLD = 20
 
 jira_options = {'server': config('JIRA_INSTANCE')}
 jira = JIRA(options = jira_options, basic_auth=(config('JIRA_EMAIL'), JIRA_API_TOKEN))
@@ -29,19 +31,17 @@ def show_projects():
 	if selected_index >= 0 and selected_index < len(projects):
 		review_tasks(projects[selected_index])
 
-def compare_by_due_date(issue1, issue2):
-	due_date1 = issue1.fields.duedate
-	due_date2 = issue2.fields.duedate
-	today = date.today()
-	if due_date1 == None and due_date2 == None:
+def compare_by_date(date1, date2):
+	if date1 == None and date2 == None:
 		return 0
-	elif due_date1 != None and due_date2 == None:
+	elif date1 != None and date2 == None:
 		return -1
-	elif due_date1 == None and due_date2 != None:
+	elif date1 == None and date2 != None:
 		return 1
 	else:
-		d1 = datetime.strptime(due_date1, "%Y-%m-%d")
-		d2 = datetime.strptime(due_date2, "%Y-%m-%d")
+		d1 = datetime.strptime(date1, "%Y-%m-%d")
+		d2 = datetime.strptime(date2, "%Y-%m-%d")
+		today = date.today()
 		today = datetime.strptime(str(today), "%Y-%m-%d")
 		delta1 = today - d1
 		delta2 = today - d2
@@ -50,58 +50,98 @@ def compare_by_due_date(issue1, issue2):
 		else:
 			return 1
 
-def filter_by_start_date(issue):
-	start_date_string = getattr(issue.fields, name_map["Start date"])
-	if start_date_string == None:
-		return True
-	start_date = datetime.strptime(start_date_string, "%Y-%m-%d").date()
-	today = date.today()
-	return start_date < today
+def compare_by_start_date(issue1, issue2):
+	start_date1 = getattr(issue1.fields, name_map["Start date"])
+	start_date2 = getattr(issue2.fields, name_map["Start date"])
+	return compare_by_date(start_date1, start_date2)
+
+def compare_by_due_date(issue1, issue2):
+	due_date1 = issue1.fields.duedate
+	due_date2 = issue2.fields.duedate
+	if due_date1 == due_date2:
+		return compare_by_start_date(issue1, issue2)
+	return compare_by_date(due_date1, due_date2)
+
+def filter_by_update_date(issue):
+	updated_date = issue.fields.updated
+	if updated_date == None:
+		return False
+
+	# updated_time = datetime.strptime(updated_date, "%Y-%m-%dT%H:%M:%S.%f%z")
+	updated_time = parser.isoparse(updated_date)
+	updated_time = updated_time.astimezone().replace(tzinfo=None)
+	now = datetime.now()
+	diff = (now - updated_time).total_seconds() / 60
+	return diff > NO_UPDATE_THRESHOLD
+
+def filter_by_status(issue):
+	return issue.fields.status != None and issue.fields.status.name != "Done"
+
+def sort_by_status(issue):
+	if issue.fields.status.name == "In Progress":
+		return 0
+	else:
+		return 1
 
 def sort_and_filter_issue(issues):
-	sorted_issues = sorted(issues, key=lambda x: x.fields.assignee.displayName)
-	grouped_issues_by_assignee = itertools.groupby(sorted_issues, key=lambda x: x.fields.assignee.displayName)
-	for assignee, group in grouped_issues_by_assignee:
-		print(f"\nAssignee: {assignee}")
-		group = sorted(group, key=cmp_to_key(compare_by_due_date))
-		group = list(filter(filter_by_start_date, group))
-		for issue in group:
-			startDate = getattr(issue.fields, name_map["Start date"])
-			print(f"Issue: {issue.fields.summary} {issue.fields.updated} {startDate}")
-	return grouped_issues_by_assignee
+	issues = sorted(issues, key=cmp_to_key(compare_by_due_date))
+	issues = list(filter(filter_by_update_date, issues))
+	issues = list(filter(filter_by_status, issues))
+	issues = sorted(issues, key=sort_by_status)
+	return issues
+
+def print_issues(issues):
+	list_all_issues = []
+	for issue in issues:
+		startDate = getattr(issue.fields, name_map["Start date"])
+		list_issue = []
+		list_issue.append(issue.key)
+		list_issue.append(issue.fields.summary)
+		list_issue.append(issue.fields.reporter.displayName)
+		list_issue.append(issue.fields.assignee)
+		list_issue.append(issue.fields.duedate)
+		list_issue.append(issue.fields.updated)
+		list_issue.append(startDate)
+		list_issue.append(issue.fields.status)
+		list_all_issues.append(list_issue)
+
+	df_issues = pd.DataFrame(list_all_issues, columns=["Key", "Summary", "Reporter", "Assignee", "Due Date", "Updated", "Start Date", "Status"])
+	df_issues.style.set_properties(**{'text-align': 'left'})
+	column_tiles = ["Key", "Summary", "Due Date", "Updated", "Start Date", "Status"]
+	df_issues = df_issues.reindex(columns=column_tiles)
+	print(df_issues)
+
+def request_update_from_assignee(PersonName, TaskData):
+	logging.info(f"request_update_from_assignee : {PersonName} - {TaskData}")
+
+def request_update_from_reporter(PersonName, TaskData):
+	logging.info(f"request_update_from_reporter : {PersonName} - {TaskData}")
 
 def review_tasks(project):
 	issues = jira.search_issues(jql_str=f"project = {project.name}")
 
-	list_all_issues = []
-	for singleIssue in issues:
-		list_issue = []
-		list_issue.append(singleIssue.key)
-		list_issue.append(singleIssue.fields.summary)
-		list_issue.append(singleIssue.fields.reporter.displayName)
-		list_issue.append(singleIssue.fields.assignee)
-		list_all_issues.append(list_issue)
-		# print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary, 
-    #                          singleIssue.fields.reporter.displayName)) 
-	df_issues = pd.DataFrame(list_all_issues, columns=["Key", "Summary", "Reporter", "Assignee"])
-	column_tiles = ["Key", "Summary", "Reporter", "Assignee"]
-	df_issues = df_issues.reindex(columns=column_tiles)
-	# logging.info(df_issues)
-
 	# Get assigned issues
 	assigned_issues = list(filter(lambda x: x.fields.assignee != None, issues))
-	# sorted_issues = sorted(assigned_issues, key=lambda x: x.fields.assignee.displayName)
-	# grouped_issues_by_assignee = itertools.groupby(sorted_issues, key=lambda x: x.fields.assignee.displayName)
-	# for assignee, group in grouped_issues_by_assignee:
-	# 	print(f"\nAssignee: {assignee}")
-	# 	for issue in group:
-	# 		print(f"Issue: {issue.fields.summary} {issue.fields.updated}")
-	
-	sort_and_filter_issue(assigned_issues)
+	sorted_issues = sorted(assigned_issues, key=lambda x: x.fields.assignee.displayName)
+	grouped_issues_by_assignee = itertools.groupby(sorted_issues, key=lambda x: x.fields.assignee.displayName)
+	for assignee, group in grouped_issues_by_assignee:
+		# print(f"\nAssignee: {assignee}")
+		group = sort_and_filter_issue(group)
+		if len(group) > 0:
+			request_update_from_assignee(assignee, group[0])
+		# print_issues(group)
+		
 
-	# non_assigned_issues = list(filter(lambda x: x.fields.assignee == None, issues))
-	# sorted_issues = sorted(non_assigned_issues, key=lambda x: x.fields.reporter.displayName)
-	# grouped_issues_by_reporter = itertools.groupby(sorted_issues, key=lambda x: x.fields.reporter.displayName)
+	non_assigned_issues = list(filter(lambda x: x.fields.assignee == None, issues))
+	sorted_issues = sorted(non_assigned_issues, key=lambda x: x.fields.reporter.displayName)
+	grouped_issues_by_reporter = itertools.groupby(sorted_issues, key=lambda x: x.fields.reporter.displayName)
+	for reporter, group in grouped_issues_by_reporter:
+		# print(f"\nReporter: {reporter}")
+		group = sort_and_filter_issue(group)
+		if len(group) > 0:
+			request_update_from_reporter(reporter, group[0])
+		# print_issues(group)
+
 	# for reporter, group in grouped_issues_by_reporter:
 	# 	print(f"Reporter: {reporter}")
 	# 	for issue in group:
