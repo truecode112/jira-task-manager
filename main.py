61857fab37a3c3fd0b60b00f8a2fc5b714c0f32f
truecode112 <truecoder112@gmaili.com>
2023-12-04 11:08:25 +0300

    sort by over due

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from decouple import config
 from jira import JIRA
 import pandas as pd
+import logging
+import itertools
+from datetime import datetime
+from datetime import date
+from functools import cmp_to_key
+
+logging.basicConfig(format="%(asctime)s \t%(message)s", level=logging.INFO)
 
 JIRA_API_TOKEN = config('JIRA_API_TOKEN')
 
@@ -9,7 +16,7 @@ jira = JIRA(options = jira_options, basic_auth=(config('JIRA_EMAIL'), JIRA_API_T
 
 def show_projects():
 	projects = jira.projects()
-	print("Available projects")
+	print(f"Available projects")
 	project_index = 0
 	for project in projects:
 		print(f"{project_index}. {project.name}")
@@ -20,6 +27,37 @@ def show_projects():
 	if selected_index >= 0 and selected_index < len(projects):
 		review_tasks(projects[selected_index])
 
+def compare_by_due_date(issue1, issue2):
+	due_date1 = issue1.fields.duedate
+	due_date2 = issue2.fields.duedate
+	today = date.today()
+	if due_date1 == None and due_date2 == None:
+		return 0
+	elif due_date1 != None and due_date2 == None:
+		return -1
+	elif due_date1 == None and due_date2 != None:
+		return 1
+	else:
+		d1 = datetime.strptime(due_date1, "%Y-%m-%d")
+		d2 = datetime.strptime(due_date2, "%Y-%m-%d")
+		today = datetime.strptime(str(today), "%Y-%m-%d")
+		delta1 = today - d1
+		delta2 = today - d2
+		if delta1 > delta2:
+			return -1
+		else:
+			return 1
+
+def sort_and_filter_issue(issues):
+	sorted_issues = sorted(issues, key=lambda x: x.fields.assignee.displayName)
+	grouped_issues_by_assignee = itertools.groupby(sorted_issues, key=lambda x: x.fields.assignee.displayName)
+	for assignee, group in grouped_issues_by_assignee:
+		print(f"\nAssignee: {assignee}")
+		group = sorted(group, key=cmp_to_key(compare_by_due_date))
+		for issue in group:
+			print(f"Issue: {issue.fields.summary} {issue.fields.duedate}")
+	return grouped_issues_by_assignee
+
 def review_tasks(project):
 	issues = jira.search_issues(jql_str=f"project = {project.name}")
 
@@ -29,13 +67,34 @@ def review_tasks(project):
 		list_issue.append(singleIssue.key)
 		list_issue.append(singleIssue.fields.summary)
 		list_issue.append(singleIssue.fields.reporter.displayName)
+		list_issue.append(singleIssue.fields.assignee)
 		list_all_issues.append(list_issue)
 		# print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary, 
     #                          singleIssue.fields.reporter.displayName)) 
-	df_issues = pd.DataFrame(list_all_issues, columns=["Key", "Summary", "Reporter"])
-	column_tiles = ["Key", "Summary", "Reporter"]
+	df_issues = pd.DataFrame(list_all_issues, columns=["Key", "Summary", "Reporter", "Assignee"])
+	column_tiles = ["Key", "Summary", "Reporter", "Assignee"]
 	df_issues = df_issues.reindex(columns=column_tiles)
-	print(df_issues)
+	# logging.info(df_issues)
+
+	# Get assigned issues
+	assigned_issues = list(filter(lambda x: x.fields.assignee != None, issues))
+	# sorted_issues = sorted(assigned_issues, key=lambda x: x.fields.assignee.displayName)
+	# grouped_issues_by_assignee = itertools.groupby(sorted_issues, key=lambda x: x.fields.assignee.displayName)
+	# for assignee, group in grouped_issues_by_assignee:
+	# 	print(f"\nAssignee: {assignee}")
+	# 	for issue in group:
+	# 		print(f"Issue: {issue.fields.summary} {issue.fields.updated}")
+	
+	sort_and_filter_issue(assigned_issues)
+
+	# non_assigned_issues = list(filter(lambda x: x.fields.assignee == None, issues))
+	# sorted_issues = sorted(non_assigned_issues, key=lambda x: x.fields.reporter.displayName)
+	# grouped_issues_by_reporter = itertools.groupby(sorted_issues, key=lambda x: x.fields.reporter.displayName)
+	# for reporter, group in grouped_issues_by_reporter:
+	# 	print(f"Reporter: {reporter}")
+	# 	for issue in group:
+	# 		print(f"Issue: {issue.fields.summary}")
+
 
 if __name__ == "__main__":
 	show_projects()
